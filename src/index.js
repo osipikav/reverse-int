e32bf276473b5024c3e4e0aaa96e558ac90824bd
андрей пользователь <you@example.com>
2022-02-23 00:32:48 +0300

    final

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    if (n < 0) {
+        n = -n;
+    }
+    var strReverse = n.toString().split("").reverse().join("");
+    // console.log("b :>> ", strReverse);
+    return strReverse;
+};
